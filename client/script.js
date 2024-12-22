a4c4ae7651ccd23ef75de8b5ae1cf7fcb3388b20
SebastianHops <h23sebho@du.se>
2024-12-22 14:26:49 +0100

    Finished

--- client/script.js
+++ client/script.js
@@ -1,36 +1,26 @@
-const url = 'http://localhost:3000/users';
+const url = 'http://localhost:3000/users'; //lagrar våran backend data i en variabel
 
-const uListElement = document.createElement('ul');
-document.body.appendChild(uListElement);
-uListElement.classList.add('list-group');
+const uListElement = document.createElement('ul'); // skapar ett nytt element
+document.body.appendChild(uListElement); // lägger till ul i domträdet i body
+uListElement.classList.add('list-group'); // lägger till en class på elementet
 
+// gör ett anrop till url för att hämta användardatan vi har
+// använder then för att hantera asynkront, den tilldelas en callbackfunktion som även innehar response som svar för datan vi får tillbaka.
+// response json, omvandlar jsonfromatet till en js array(objekt), för att göra det möjligt att göra något med datan
 fetch(url)
   .then((response) => {
     console.log(response);
     return response.json();
   })
+  // tar emot datan vilket är users med then metod och en callbackfunktion. använder console.log för att säkerställa att vi fått rätt data, sedan loopar vi igenom arrayen users och för varje iteration så skapas ett list-group-item.
   .then((users) => {
     console.log(users);
     users.forEach((user) => {
       const listElement = document.createElement('li');
       listElement.classList.add('list-group-item');
-      listElement.textContent = `${user.firstName} ${user.lastName} ${user.username}`;
-      listElement.style.background = user.color;
-      /*const html = `<p>${user.firstName + ' ' + user.lastName}</p>`;
-      document.body.insertAdjacentHTML('beforeend', html); */
+      listElement.textContent = `${user.firstName} ${user.lastName} ${user.username}`; // i textcontent på elementet läggs info för varje user till
+      listElement.style.background = user.color; // stylar varje users element med hjälp av style och preferens på users färgval.
       console.log(listElement);
-      /*document.body.appendChild(listElement);*/
-      uListElement.appendChild(listElement);
+      uListElement.appendChild(listElement); //lägger till li elementet i ul elemenet i domträdet
     });
   });
-
-/*
- fetch(url).then((response) => console.log(response));
-
-const request = new Request('http://localhost:3000/users');
-
-console.log(request);
-
-async function fetchData() {
-  const response = await fetch(request);
-} */
--- server/server.js
+++ server/server.js
@@ -1,26 +1,30 @@
-const express = require('express');
-const server = express();
-const cors = require('cors'); // Vi fick lov att installera cors för att det skulle fungera
+const express = require('express'); // Används för att skapa en webbserver
+const server = express(); // Skapar en instans av servern
+const sqlite3 = require('sqlite3').verbose(); //sqlite används för att lagra databas utan att kräva en separat server
 
-const sqlite3 = require('sqlite3').verbose();
-server.use(cors());
-
+// Övergripande inställningar för servern
 server
-  .use(express.json())
+  .use(express.json()) // Gör så att servern kan servern kan hantera json-data
   .use(express.urlencoded({ extended: false }))
   .use((req, res, next) => {
+    // CORS inställningar för att tillåta förfrågningar från andra domäner
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Methods', '*');
     next();
   });
+
+// Hanterar get förfrågningar till /users
 server.get('/users', (req, res) => {
-  const db = new sqlite3.Database('./gik339-labb2.db');
-  const sql = 'Select * FROM users';
+  const db = new sqlite3.Database('./gik339-labb2.db'); // Skapar en anslutning till lokala databasen
+  const sql = 'Select * FROM users'; //SQL fråga, tar alla rader från users tabellen
   db.all(sql, (err, rows) => {
+    // Hämtar all data från sql variabeln och skickar resultatet (rows) till klienten i json format om frågan lyckas, annars skickas ett error meddelande
     res.send(rows);
   });
 });
 
+// Startar servern och på port 3000
 server.listen(3000, () => {
   console.log('server running on http://localhost:3000');
 });
